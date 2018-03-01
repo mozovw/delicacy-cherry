73bd1f1f5e0dec2770d95e59b09b34df0c4e97e6
张玉涛 <zhangyutao@tomato.shop>
2018-03-01 15:20:05 +0800

    cherry-config

---
+++ cherry-config/cherry-config-client/src/main/java/com/delicacy/cherry/config/client/CherryConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.delicacy.cherry.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherryConfigClientApplication.class, args);
+	}
+}
