36bfbd6652e93d12fa4021a1fec9f0de8cdb4813
张玉涛 <zhangyutao@tomato.shop>
2018-04-13 18:15:59 +0800

    input_group

---
+++ cherry-stream/cherry-stream-rabbitmq-consumer/src/main/java/com/delicacy/cherry/stream/rabbitmq/consumer/CherryStreamRabbitmqConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.delicacy.cherry.stream.rabbitmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryStreamRabbitmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherryStreamRabbitmqConsumerApplication.class, args);
+	}
+}
---
+++ cherry-stream/cherry-stream-rabbitmq-producer/src/main/java/com/delicacy/cherry/stream/rabbitmq/producer/CherryStreamRabbitmqProducerApplication.java
@@ -0,0 +1,11 @@
+package com.delicacy.cherry.stream.rabbitmq.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryStreamRabbitmqProducerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CherryStreamRabbitmqProducerApplication.class, args);
+	}
+}
